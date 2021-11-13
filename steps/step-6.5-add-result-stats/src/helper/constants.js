ad5eda21f5bd45054ae307bd2f265a9bbea12953
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-11-13 18:50:28 +0530

    feat: adding step 6.5 - add result stats

---
+++ steps/step-6.5-add-result-stats/src/helper/constants.js
@@ -0,0 +1,11 @@
+export const STATIC_RECIPE_SUGGESTIONS = [
+  { value: "Pizza", tagColor: "magenta" },
+  { value: "Curry", tagColor: "red" },
+  { value: "Low Carb", tagColor: "volcano" },
+  { value: "Pasta", tagColor: "orange" },
+  { value: "Fried", tagColor: "gold" },
+  { value: "Diabetic", tagColor: "lime" },
+  { value: "Veg", tagColor: "green" },
+  { value: "Healthy", tagColor: "geekblue" },
+  { value: "Saute", tagColor: "cyan" },
+];
